f55d5a43e56e58f3af00b44ced65684bef7029f9
Daniel Ryland <rylandcs@gmail.com>
2023-01-20 12:12:00 -0600

    Boolean & Conditional Logic

---
+++ Booleans-&-Conditional-Logic/index.js
@@ -0,0 +1,9 @@
+let currentLightColor = "Green";
+
+if (currentLightColor === "Green") {
+    console.log("The light is Green.");
+} else if (currentLightColor === "Yellow") {
+    console.log("The light is Yellow.");
+} else {
+    console.log("The light is Red.");
+}
