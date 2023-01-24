8895f420a07dd5d0ec15d3fdfe2736c2c40ec8d7
Daniel Ryland <rylandcs@gmail.com>
2023-01-24 12:04:46 -0600

    Arrays and console logs that go with the arrays

---
+++ Arrays/index.js
@@ -0,0 +1,19 @@
+// Javascript arrays = Arrays are of "type object"
+
+var tvShows = [
+  'One Punch Man', 
+  'Bleach', 
+  'The Witcher', 
+  'Lord of the Rings'
+];
+
+
+var userSaveMovies = [];
+
+userSaveMovies.push('The Rocketeer');
+userSaveMovies.push('Fifel goes West');
+userSaveMovies.push('Toy Story');
+
+userSaveMovies.pop();
+
+console.log(userSaveMovies.length); // This Selects different values in an array can do it in numerical order
